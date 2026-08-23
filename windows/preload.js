ac5b353e30ac6e9f05aa61e2b97bb9882c7e8936
Minh Tu Nguyen <r3621721279@163.com>
2026-08-23 22:34:09 +0800

    Add windows/preload.js

---
+++ windows/preload.js
@@ -0,0 +1,3 @@
+const {contextBridge,ipcRenderer}=require('electron');
+contextBridge.exposeInMainWorld('DesktopTTS',{generate:text=>ipcRenderer.invoke('tts-generate',text),open:url=>ipcRenderer.invoke('open-url',url)});
+
